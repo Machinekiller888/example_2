efbe451224b1d5475cb7e69c07999432dd36441e
Nurak <machine_killer@rmutl.ac.th>
2024-05-16 16:33:35 +0700

    เพิ่มปุ่ม + สำหรับเพิ่มค่าตัวแปร counter

---
+++ lib/screen/my_home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My increment counter"),
+      ),
+      body: Center(
+        child: Text("Counted: $_counter"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
